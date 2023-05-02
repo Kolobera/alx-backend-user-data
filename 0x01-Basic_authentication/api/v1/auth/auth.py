96e78d327f2584ee79f64e1425beab540957824f
Ousmane Konate <konatealim@gmail.com>
2023-05-02 19:36:11 +0200

    add task 10

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,13 +10,18 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require auth method
+        allowing * at the end of excluded paths
         """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
         if path[-1] != '/':
             path += '/'
-        if path in excluded_paths:
-            return False
+        for p in excluded_paths:
+            if p[-1] == '*':
+                if path.startswith(p[:-1]):
+                    return False
+            elif path == p:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:

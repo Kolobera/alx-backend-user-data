910b242198eac95143af76325864d14c5ebdf2fb
Ousmane Konate <konatealim@gmail.com>
2023-05-02 19:21:07 +0200

    add task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module for API authentication
 """
-from typing import List, TypeVar
+from typing import List, TypeVar, Tuple
 from .auth import Auth
 import base64
 import binascii
@@ -33,3 +33,14 @@ class BasicAuth(Auth):
                 base64_authorization_header).decode('utf-8')
         except binascii.Error:
             return None
+        
+    def extract_user_credentials(self,
+                                    decoded_base64_authorization_header: str
+                                    ) -> Tuple['str', 'str']:
+            """ Extract user credentials method
+            """
+            if decoded_base64_authorization_header is None or \
+            type(decoded_base64_authorization_header) is not str or \
+            ':' not in decoded_base64_authorization_header:
+                return (None, None)
+            return tuple(decoded_base64_authorization_header.split(':', 1))

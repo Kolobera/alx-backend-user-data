265813be61dca7e87f163d2cdaa720b1a74b024e
Ousmane Konate <konatealim@gmail.com>
2023-05-02 19:06:09 +0200

    add task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,17 @@ from typing import List, TypeVar
 from auth import Auth
 
 
+
+
 class BasicAuth(Auth):
     """ BasicAuth class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extract base64 authorization header method
+        """
+        if authorization_header is None or \
+           type(authorization_header) is not str or \
+           not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]

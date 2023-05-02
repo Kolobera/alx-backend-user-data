8d47f249e360b71ae4d4383ca087b9fb3e80277a
Ousmane Konate <konatealim@gmail.com>
2023-05-02 19:10:14 +0200

    add task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 """ Module for API authentication
 """
-from flask import request
 from typing import List, TypeVar
-from auth import Auth
-
-
+from .auth import Auth
 
 
 class BasicAuth(Auth):

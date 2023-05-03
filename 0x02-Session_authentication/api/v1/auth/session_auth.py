399e8960b09a6c64a50710e573401834c54d294f
Ousmane Konate <konatealim@gmail.com>
2023-05-03 17:07:49 +0200

    add Task 1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module for Session authentication
 """
-from api.v1.auth.auth import Auth
+from .auth import Auth
 from models.user_session import UserSession
 from uuid import uuid4
 

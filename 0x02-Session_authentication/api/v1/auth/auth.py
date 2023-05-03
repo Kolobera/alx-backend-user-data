91d023ce1a5f8d18c8e0bdfa4ea451a17fbc281f
Ousmane Konate <konatealim@gmail.com>
2023-05-03 16:44:50 +0200

    add Task 0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Module for API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require auth method
+        allowing * at the end of excluded paths
+        """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for p in excluded_paths:
+            if p[-1] == '*':
+                if path.startswith(p[:-1]):
+                    return False
+            elif path == p:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Authorization header method
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current user method
+        """
+        return None

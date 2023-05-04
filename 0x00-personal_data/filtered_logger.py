9a06bcf5dd1724f9ed6d92eb94d399d4649cb8ed
Ousmane Konate <konatealim@gmail.com>
2023-05-04 16:21:25 +0200

    add Task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,15 +3,27 @@
 import re
 from typing import List
 import logging
+import os
+import mysql.connector
 
 
 def filter_datum(fields: List[str], redaction: str,
-                    message: str, separator: str) -> str:
-        """Returns the log message obfuscated"""
-        for field in fields:
-            message = re.sub(f'{field}=.+?{separator}',
-                                f'{field}={redaction}{separator}', message)
-        return message
+                 message: str, separator: str) -> str:
+    """Returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(f'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
+
+
+def get_db(self):
+        """Returns a database connection"""
+        return mysql.connector.connect(
+            host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+            database=os.getenv('PERSONAL_DATA_DB_NAME', ''),
+            user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+            password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        )
 
 
 class RedactingFormatter(logging.Formatter):

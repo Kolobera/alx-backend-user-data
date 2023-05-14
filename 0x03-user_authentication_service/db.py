da5e69e98c7f93c88d7ccd5603bdc92db00957b8
Ousmane Konate <konatealim@gmail.com>
2023-05-14 03:15:57 +0200

    add Task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,4 +39,19 @@ class DB:
         user.hashed_password = hashed_password
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """returns the first row found in the users table
+        as filtered by the method’s input arguments"""
+        keyword = list(kwargs.items())[0][0]
+        try:
+            user = self._session.query(User).filter(
+                getattr(User, keyword) == kwargs[keyword]).first()
+        except InvalidRequestError:
+            raise InvalidRequestError
+        except AttributeError:
+            raise NoResultFound
+        if user is None:
+            raise NoResultFound
         return user

18cbf451c7f50c543c468a4e27339686dafe0507
Ousmane Konate <konatealim@gmail.com>
2023-05-14 18:18:38 +0200

    add Task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,7 +62,8 @@ class Auth:
             user = self._db.find_user_by(email=email)
             if user:
                 session_id = _generate_uuid()
-                self._db.update_user(f"session_id={session_id}")
+                self._db.update_user(user.id, session_id=session_id)
                 return session_id
-        except Exception as e:
-            pass
+            return None
+        except NoResultFound:
+            return None

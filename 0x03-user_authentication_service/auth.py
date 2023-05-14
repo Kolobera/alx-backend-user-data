46f3703a86bc357c9eb9f488b5efbfa3cccc8947
Ousmane Konate <konatealim@gmail.com>
2023-05-14 17:57:22 +0200

    add Task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,3 +30,24 @@ class Auth:
                                      hashed_password=hashed_password)
         if user is not None:
             raise ValueError(f"User {email} already exists")
+    
+    def valid_login(self, email: str, password: str) -> bool:
+        """ry locating the user by email.
+        If it exists, check the password with bcrypt.checkpw.
+        If it matches return True.
+        In any other case, return False."""
+        if email is None or password is None:
+            return None
+        try:
+            user = self._db.find_user_by(email=email)
+            encoded_password = password.encode('utf-8')
+            if user:
+                if bcrypt.checkpw(encoded_password, user.hashed_password):
+                    return True
+                return False
+            return False
+        except NoResultFound:
+            return False
+        
+
+
